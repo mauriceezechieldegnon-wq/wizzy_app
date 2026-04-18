2867ae01b0bd527a01b0299ba464ef0ebcabcd28
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-04-18 15:02:30 +0200

    Update splash screen title to 'WIZZY V2'

--- lib/features/auth/screens/splash_screen.dart
+++ lib/features/auth/screens/splash_screen.dart
@@ -69,7 +69,7 @@ class _WizzySplashScreenState extends State<WizzySplashScreen> {
                     ),
                     const SizedBox(height: 25),
                     const Text(
-                      "WIZZY",
+                      "WIZZY V2",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 36,
