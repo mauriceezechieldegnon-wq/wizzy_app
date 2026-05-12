151ea45d8196bb6aeccd0dcbc252830a9f093c4e
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-05-12 17:56:36 +0200

    Modify color constants for AppColors
    Updated background and surface colors for dark mode.

--- lib/features/core/constants/app_colors.dart
+++ lib/features/core/constants/app_colors.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryPurple = Color(0xFF6200EE); // Violet Électrique
-  static const Color accentYellow = Color(0xFFEEFF41); // Jaune Néon
-  static const Color backgroundBlack = Color(
-    0xFF121212,
-  ); // Pour un mode sombre tendance
-  static const Color surfaceGrey = Color(0xFF1E1E1E);
+  static const Color primaryPurple = Color(0xFF6200EE);
+  static const Color accentYellow = Color(0xFFEEFF41);
+  static const Color backgroundBlack = Color(0xFF09090B);
+  static const Color surfaceGrey = Color(0xFF1A1A1A);
 }
