5fb2be8a3c73a480a2d0b2c2964b8d27e3db6b8e
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-05-18 17:32:04 +0200

    Update register_screen.dart

--- lib/features/auth/screens/register_screen.dart
+++ lib/features/auth/screens/register_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:wizzy/core/constants/app_colors.dart';
+import 'package:wizzy/core/constants/app_colors.dart'; // IMPORT CORRIGÉ
 import 'package:wizzy/features/auth/services/auth_service.dart';
 
 class RegisterScreen extends StatefulWidget {
