6d1810face115c155cb449556fa4c16a38f47cbd
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-05-18 17:19:11 +0200

    Update app_colors.dart

--- lib/features/core/constants/app_colors.dart
+++ lib/features/core/constants/app_colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 class AppColors {
   static const Color primaryPurple = Color(0xFF6200EE);
   static const Color accentYellow = Color(0xFFEEFF41);
   static const Color backgroundBlack = Color(0xFF09090B);
+  static const Color surfaceGrey = Color(0xFF1A1A1A);
 }
